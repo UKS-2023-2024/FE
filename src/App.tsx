d89a3ad988fa0af03cf86b400d7a5d2a90463853
srdjan1210 <businessrdjan@gmail.com>
2023-10-30 17:27:00 +0100

    Added tanstack query

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
 import { Menu } from "./components/navigation/Menu";
-import { Toaster, useToast } from "./components/toast";
-import { useEffect } from "react";
+import { Toaster } from "./components/toast";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
-  const { toast } = useToast();
-
-  useEffect(() => {
-    console.log("component has mounted");
-    toast({
-      description: "Component has mounted",
-    });
-  }, []);
   return (
-    <div className="h-screen w-full">
-      <Menu />
-      <div className="h-[95%]">
-        <Outlet />
+    <QueryClientProvider client={queryClient}>
+      <div className="h-screen w-full">
+        <Menu />
+        <div className="h-[95%]">
+          <Outlet />
+        </div>
+        <Toaster />
       </div>
-      <Toaster />
-    </div>
+    </QueryClientProvider>
   );
 }
 
