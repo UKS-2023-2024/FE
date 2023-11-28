b637f25b871a6afcd158f831c87af54ab400592e
Dusan Janosevic <dusan.janosevic123@gmail.com>
2023-11-28 18:31:49 +0100

    finished changing repository member role

---
+++ src/modules/repository/model/ChangeRepositoryMemberRoleParams.ts
@@ -0,0 +1,7 @@
+import { RepositoryMemberRole } from "../../../store/model/repositoryMember.model";
+
+export type ChangeRepositoryMemberRoleParams = {
+  repositoryMemberId: string;
+  repositoryId: string;
+  repositoryRole: RepositoryMemberRole
+};
