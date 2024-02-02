e4237ea11f3aeac7c5eebb7d79f14a451e72d3f1
Dusan Janosevic <dusan.janosevic123@gmail.com>
2024-02-02 14:06:01 +0100

    finished refactoring for org and repo members

---
+++ src/components/select/index.tsx
@@ -0,0 +1,10 @@
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "./select";
+export { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue };
